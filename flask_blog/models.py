6a3035dd3d72ae8621915f5b5631f6b984fb2675
TriickShotz <matthew@mullinsfamily.ca>
2020-12-14 17:10:26 -0700

    Create database file

--- flask_blog/models.py
+++ flask_blog/models.py
@@ -2,6 +2,9 @@ from datetime import datetime
 from flask_blog import db
 
 class User(db.Model):
+    """
+    User class to represent a table in the database
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -13,6 +16,9 @@ class User(db.Model):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
+    """
+    Post class to represent a table in the database
+    """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -21,4 +27,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
-        
